848f297408b1711093e144b06741d641a6d0cbfc
wangzhen0625 <13521146683@163.com>
2019-05-21 15:53:31 +0800

    1

--- 22img/main.go
+++ 22img/main.go
@@ -2,18 +2,52 @@ package main
 
 import (
 	"bufio"
+	"crypto/tls"
 	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
-	_ "image/gif"
-    _ "image/jpeg"
-	_ "image/png"
-	"image"
 )
 
+// 获取在线图片
+func GetOnlineImage() {
+	// var url = "https://pic2016.ytqmx.com/img/20180704/qfvzsyartbg.jpg"
+	var url = "https://img3.doubanio.com/view/group_topic/large/public/p104935104.jpg"
+	// var url = "https://gss3.bdstatic.com/-Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike150%2C5%2C5%2C150%2C50/sign=389ad835d158ccbf0fb1bd6878b1d75b/fd039245d688d43f0cd0e0757d1ed21b0ef43b3f.jpg"
+	//跳过证书验证
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Printf("Fail exec http client Do,err:%s\n", err.Error())
+	}
+	fmt.Println("resp.Body:", resp.ContentLength)
+	f, err := os.Create("wz.jpg")
+	if err != nil {
+		panic(err)
+	}
+	wlen, err := io.Copy(f, resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("wlen:", wlen)
+	f.Close()
+}
+
 var filePth = "./2.jpg"
-func main(){
+
+func main() {
+	GetOnlineImage()
+
+	return
 	f, err := os.Open(filePth)
 	if err != nil {
 		fmt.Println("err1:", err)
@@ -27,8 +61,8 @@ func main(){
 	// fmt.Println("height = ", c.Height)
 
 	img, s2, err := image.Decode(f)
-	fmt.Printf("%+v\n",img.ColorModel())
-	fmt.Printf("%+v\n",img.Bounds())
+	fmt.Printf("%+v\n", img.ColorModel())
+	fmt.Printf("%+v\n", img.Bounds())
 	fmt.Println("s2 = ", s2)
 }
 func main2() {
