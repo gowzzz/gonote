34bedeb280df7b72d739c8a795c3dd763190dfad
wangzhen0625 <13521146683@163.com>
2019-09-12 17:57:28 +0800

    1

--- websocket/main.go
+++ websocket/main.go
@@ -1,83 +1,84 @@
 package main
 
-import(
-	"github.com/gorilla/websocket"
-	"log"
-	"net/url"
+import (
+	"context"
+	"crypto/tls"
 	"flag"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
+
 //  connectex: No connection could be made because the target machine actively refused it.  服务器没启动
 var addr = flag.String("addr", "localhost:8080", "http service address")
-func main(){
+
+func main() {
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
-	websocket.DefaultDialer.HandshakeTimeout=5 * time.Second
+	websocket.DefaultDialer.HandshakeTimeout = 3 * time.Second
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
 	defer c.Close()
 }
-// var cstDialer = Dialer{
-// 	Subprotocols:     []string{"p1", "p2"},
-// 	ReadBufferSize:   1024,
-// 	WriteBufferSize:  1024,
-// 	HandshakeTimeout: 30 * time.Second,
-// }
-// // DefaultDialer is a dialer with all fields set to the default values.
-// var DefaultDialer = &Dialer{
-// 	Proxy:            http.ProxyFromEnvironment,
-// 	HandshakeTimeout: 45 * time.Second,
-// }
-// type Dialer struct {
-// 	// NetDial specifies the dial function for creating TCP connections. If
-// 	// NetDial is nil, net.Dial is used.
-// 	NetDial func(network, addr string) (net.Conn, error)
 
-// 	// NetDialContext specifies the dial function for creating TCP connections. If
-// 	// NetDialContext is nil, net.DialContext is used.
-// 	NetDialContext func(ctx context.Context, network, addr string) (net.Conn, error)
+var cstDialer = Dialer{
+	Subprotocols:     []string{"p1", "p2"},
+	ReadBufferSize:   1024,
+	WriteBufferSize:  1024,
+	HandshakeTimeout: 30 * time.Second,
+}
 
-// 	// Proxy specifies a function to return a proxy for a given
-// 	// Request. If the function returns a non-nil error, the
-// 	// request is aborted with the provided error.
-// 	// If Proxy is nil or returns a nil *URL, no proxy is used.
-// 	Proxy func(*http.Request) (*url.URL, error)
+// DefaultDialer is a dialer with all fields set to the default values.
+var DefaultDialer = &Dialer{
+	Proxy:            http.ProxyFromEnvironment,
+	HandshakeTimeout: 45 * time.Second,
+}
 
-// 	// TLSClientConfig specifies the TLS configuration to use with tls.Client.
-// 	// If nil, the default configuration is used.
-// 	TLSClientConfig *tls.Config
+type Dialer struct {
+	// NetDial specifies the dial function for creating TCP connections. If
+	// NetDial is nil, net.Dial is used.
+	NetDial func(network, addr string) (net.Conn, error)
 
-// 	// HandshakeTimeout specifies the duration for the handshake to complete.
-// 	HandshakeTimeout time.Duration
+	// NetDialContext specifies the dial function for creating TCP connections. If
+	// NetDialContext is nil, net.DialContext is used.
+	NetDialContext func(ctx context.Context, network, addr string) (net.Conn, error)
 
-// 	// ReadBufferSize and WriteBufferSize specify I/O buffer sizes in bytes. If a buffer
-// 	// size is zero, then a useful default size is used. The I/O buffer sizes
-// 	// do not limit the size of the messages that can be sent or received.
-// 	ReadBufferSize, WriteBufferSize int
+	// Proxy specifies a function to return a proxy for a given Request.
+	//If the function returns a non-nil error, the request is aborted with the provided error.
+	// If Proxy is nil or returns a nil *URL, no proxy is used.
+	Proxy func(*http.Request) (*url.URL, error)
 
-// 	// WriteBufferPool is a pool of buffers for write operations. If the value
-// 	// is not set, then write buffers are allocated to the connection for the
-// 	// lifetime of the connection.
-// 	//
-// 	// A pool is most useful when the application has a modest volume of writes
-// 	// across a large number of connections.
-// 	//
-// 	// Applications should use a single pool for each unique value of
-// 	// WriteBufferSize.
-// 	WriteBufferPool BufferPool
+	// TLSClientConfig specifies the TLS configuration to use with tls.Client.
+	// If nil, the default configuration is used.
+	TLSClientConfig *tls.Config
 
-// 	// Subprotocols specifies the client's requested subprotocols.
-// 	Subprotocols []string
+	// HandshakeTimeout specifies the duration for the handshake to complete.
+	HandshakeTimeout time.Duration
 
-// 	// EnableCompression specifies if the client should attempt to negotiate
-// 	// per message compression (RFC 7692). Setting this value to true does not
-// 	// guarantee that compression will be supported. Currently only "no context
-// 	// takeover" modes are supported.
-// 	EnableCompression bool
+	// ReadBufferSize and WriteBufferSize specify I/O buffer sizes in bytes. If a buffer size is zero, then a useful default size is used.
+	//The I/O buffer sizes do not limit the size of the messages that can be sent or received.
+	ReadBufferSize, WriteBufferSize int
 
-// 	// Jar specifies the cookie jar.
-// 	// If Jar is nil, cookies are not sent in requests and ignored
-// 	// in responses.
-// 	Jar http.CookieJar
-// }
+	// WriteBufferPool is a pool of buffers for write operations.
+	//If the value is not set, then write buffers are allocated to the connection for the lifetime of the connection.
+	//
+	// A pool is most useful when the application has a modest volume of writes across a large number of connections.
+	//
+	// Applications should use a single pool for each unique value of  WriteBufferSize.
+	WriteBufferPool BufferPool
+
+	// Subprotocols specifies the client's requested subprotocols.
+	Subprotocols []string
+
+	// EnableCompression specifies if the client should attempt to negotiate per message compression (RFC 7692).
+	//Setting this value to true does not guarantee that compression will be supported. Currently only "no context takeover" modes are supported.
+	EnableCompression bool
+
+	// Jar specifies the cookie jar. If Jar is nil, cookies are not sent in requests and ignored in responses.
+	Jar http.CookieJar
+}
